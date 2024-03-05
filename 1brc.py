e7e9f8f1eea42683ab69aa8b79735b6fe2c34a7f
Harm Thordsen <harm.thordsen@contact-software.com>
2024-03-05 11:40:25 +0100

    Baseline for testing

--- 1brc.py
+++ 1brc.py
@@ -1,38 +1,60 @@
 import time
+import fileinput
 
-time_start = time.perf_counter()
-with open("measurements.txt", "r") as document:
-    city_temp = {}
+FILENAME = "hundred_mil.txt"
 
-    for line in document:
-          x = line.strip().split(";")
-          city = x[0]
-          temperature = round(float(x[1]), 1)
-            
-          # Entry for this city doesn't exist yet so has to be created
-          if city not in city_temp:
-               min = max = sum = avg = temperature
-               count = 1
-               city_temp[city] = (min, max, sum, count, avg)
-          # All other cases
-          else:
-               min = city_temp[city][0]
-               max = city_temp[city][1]
-               sum = city_temp[city][2]
-               count = city_temp[city][3]
-               avg = city_temp[city][4]
-               sum += temperature
-               count += 1
-               avg = (sum / count)
+def testing():
+     time_start = time.perf_counter()
 
-               if temperature < min:
-                    min = temperature
-                    city_temp[city] = (min, max, sum, count, avg)
-               elif temperature > max:
-                    max = temperature
-                    city_temp[city] = (min, max, sum, count, avg)
+     with fileinput.input(FILENAME, encoding="utf-8") as file:
+          for line in file:
+               continue
+
+     time_end = time.perf_counter()
+     print(time_end-time_start)
+
+# TODO: Use class, object for each dict entry
+def real_thing():
+     time_start = time.perf_counter()
+     with fileinput.input(FILENAME, encoding="utf-8") as file:
+          for line in file:
+               city_temp = {}
+               x = line.strip().split(";")
+               city = x[0]
+               temperature = round(float(x[1]), 1)
                     
-               city_temp[city] = (min, max, sum, count, avg)
+               # Entry for this city doesn't exist yet so has to be created
+               if city not in city_temp:
+                    min = max = sum = avg = temperature
+                    count = 1
+                    city_temp[city] = (min, max, sum, count, avg)
+               # All other cases
+               else:
+                    min = city_temp[city][0]
+                    max = city_temp[city][1]
+                    sum = city_temp[city][2]
+                    count = city_temp[city][3]
+                    avg = city_temp[city][4]
+                    sum += temperature
+                    count += 1
+                    avg = (sum / count)
 
-time_end = time.perf_counter()
-print (time_end-time_start)
+                    if temperature < min:
+                         min = temperature
+                         city_temp[city] = (min, max, sum, count, avg)
+                    elif temperature > max:
+                         max = temperature
+                         city_temp[city] = (min, max, sum, count, avg)
+                         
+                    city_temp[city] = (min, max, sum, count, avg)
+
+     time_end = time.perf_counter()
+     print(time_end-time_start)
+
+
+def main():
+     testing()
+
+
+if __name__ == "__main__":
+     main()
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # 1brc_python
-The 1 billion row challenge from [gunnarmorling](https://github.com/gunnarmorling/1brc)) in python
+
+The 1 billion row challenge from [gunnarmorling](https://github.com/gunnarmorling/1brc) in python
+
+## Duration
+
+Tested on 100mil due to time constraints. Rough overview, not really comparable.
+
+### Baseline
+
+My first idea, no real optimizations. Reading line by line.
